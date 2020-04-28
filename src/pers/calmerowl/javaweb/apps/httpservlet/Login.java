525302da1dccdfaf2cd7b1eca579d27c122e2d86
calmerowl <18779187480@163.com>
2020-04-28 10:17:01 +0800

    BeanUtils工具使用

--- src/pers/calmerowl/javaweb/apps/httpservlet/Login.java
+++ src/pers/calmerowl/javaweb/apps/httpservlet/Login.java
@@ -1,5 +1,6 @@
 package pers.calmerowl.javaweb.apps.httpservlet;
 
+import org.apache.commons.beanutils.BeanUtils;
 import pers.calmerowl.javaweb.apps.dao.UserDao;
 import pers.calmerowl.javaweb.apps.domain.User;
 
@@ -9,7 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
+import java.util.Map;
 
 @WebServlet("/Login")
 public class Login extends HttpServlet {
@@ -17,13 +20,25 @@ public class Login extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //1.设置编码
         req.setCharacterEncoding("utf-8");
-        //2.获取请求参数
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
-        //3.封装User对象
-        User loginUser = new User();
-        loginUser.setUsername(username);
-        loginUser.setPassword(password);
+//        //2.获取请求参数
+//        String username = req.getParameter("username");
+//        String password = req.getParameter("password");
+//        //3.封装User对象
+//        User loginUser = new User();
+//        loginUser.setUsername(username);
+//        loginUser.setPassword(password);
+        //2获取所有请求参数
+        Map<String , String[]> map = req.getParameterMap();
+        //3.创建User对象
+        User loginUser =new User();
+        //3.1使用BeanUtils封装
+        try {
+            BeanUtils.populate(loginUser,map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
         //4.调用UserDao的login方法
         UserDao userDao = new UserDao();
         User user = null;
